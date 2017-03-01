09fc9803e6c72cec57ea914de16c30c119ad3747
Ben Sidelinger <ben_sidelinger@discovery.com>
2017-02-28 18:28:13 -0800

    Allow custom attributes to be passed to functions

--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -3,6 +3,7 @@ import gtmParts from '../../src/index.jsx';
 
 const gtmId = 'foobar-123';
 const parts = gtmParts({ id: gtmId });
+const nonce = '2345wertasf';
 
 describe('The function GTMParts', () => {
     it('should return a noscript component', () => {
@@ -16,7 +17,7 @@ describe('The function GTMParts', () => {
     it('should return a noscript string', () => {
         const noscript = parts.noScriptAsHTML();
 
-        expect(noscript).to.startWith('<noscript>');
+        expect(noscript).to.startWith('<noscript');
         expect(noscript).to.endWith('</noscript>');
     });
 
@@ -31,7 +32,21 @@ describe('The function GTMParts', () => {
     it('should return a script string', () => {
         const script = parts.scriptAsHTML();
 
-        expect(script).to.startWith('<script>');
+        expect(script).to.startWith('<script');
         expect(script).to.endWith('</script>');
+    });
+
+    it('should add attributes if passed', () => {
+        const noscriptWithAttributes = parts.noScriptAsReact({ attributes: { nonce } });
+        expect(noscriptWithAttributes).to.have.deep.property('props.nonce', nonce);
+
+        const noscriptStringWithAttributes = parts.noScriptAsHTML({ attributes: { nonce } });
+        expect(noscriptStringWithAttributes).to.contain(`nonce="${nonce}"`);
+
+        const scriptWithAttributes = parts.scriptAsReact({ attributes: { nonce } });
+        expect(scriptWithAttributes).to.have.deep.property('props.nonce', nonce);
+
+        const scriptStringWithAttributes = parts.scriptAsHTML({ attributes: { nonce } });
+        expect(scriptStringWithAttributes).to.contain(`nonce="${nonce}"`);
     });
 });
