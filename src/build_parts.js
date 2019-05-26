83f9fb60ff7ca4c41f6428070e6d60528e4a6079
Sumit Jolly <sumit.jolly@infinitec.solutions>
2019-05-26 20:46:28 +0200

    2.2.2

--- src/build_parts.js
+++ src/build_parts.js
@@ -1,17 +1,18 @@
 function convertToKeyValueString(obj) {
-  return JSON.stringify(obj).slice(1, -1);
+    return JSON.stringify(obj).slice(1, -1);
 }
 
-function buildParts({ id, dataLayerName = 'dataLayer', additionalEvents = {}, scheme = '', previewVariables, authId = '' }) {
-  if (id === undefined) {
-      throw new Error('No GTM id provided');
-  }
+function buildParts({ id, dataLayerName = 'dataLayer', additionalEvents = {}, scheme = '',
+previewVariables, authId = '' }) {
+    if (id === undefined) {
+        throw new Error('No GTM id provided');
+    }
 
-  const iframe = `
+    const iframe = `
       <iframe src="${scheme}//www.googletagmanager.com/ns.html?id=${id}"
           height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 
-  const script = `
+    const script = `
       (function(w,d,s,l,i,a){w[l]=w[l]||[];
           w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${convertToKeyValueString(additionalEvents)}});
           var f=d.getElementsByTagName(s)[0],a=a?'&gtm_auth='+a:'',j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
@@ -19,10 +20,10 @@ function buildParts({ id, dataLayerName = 'dataLayer', additionalEvents = {}, sc
           ${previewVariables ? `+"${previewVariables}"` : ''};
           f.parentNode.insertBefore(j,f);
       })(window,document,'script','${dataLayerName}','${id}','${authId}');`;
-  return {
-      iframe,
-      script
-  };
+    return {
+        iframe,
+        script
+    };
 }
 
 export default buildParts;
